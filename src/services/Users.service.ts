046c2e19ad3c4c52de42b1626ed710a539c0fce3
Henrique Barros <henrique1_barros@hotmail.com>
2022-11-15 21:21:03 -0300

    feat: user service

--- src/services/Users.service.ts
+++ src/services/Users.service.ts
@@ -3,7 +3,8 @@ import { User } from "../entities/User";
 import { AppError } from "../errors/AppError";
 import * as bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { IUserRequest } from "./interfaces";
+import { IUserRequest } from "../interfaces";
+import { Account } from "../entities/Account";
 
 class UserService {
     static async listUsersService() {
@@ -13,9 +14,9 @@ class UserService {
     }
 
     static async createUsersService(data: IUserRequest) {
-        const manager = AppDataSource.getRepository(User)
+        const UserManager = AppDataSource.getRepository(User)
 
-        const findUserByUsername = await manager.findOneBy({
+        const findUserByUsername = await UserManager.findOneBy({
             username: data.username,
         });
 
@@ -27,9 +28,17 @@ class UserService {
 
         data.password = hashedPassword
 
-        const user = manager.create(data)
+        const user = UserManager.create(data)
 
-        await manager.save(user)
+        const AccountManager = AppDataSource.getRepository(Account)
+        
+        const account = new Account()
+        
+        await AccountManager.save(account)
+
+        user.account = account
+
+        await UserManager.save(user)
 
         return user
     }
